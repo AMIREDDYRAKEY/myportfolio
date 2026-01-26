42482dea67e7b8e83f50e0f1b0ee377f1a36185c
AMIREDDYRAKEY <rakeyr213@gmail.com>
2026-01-26 10:50:21 +0530

    seo

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,10 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
+// import ReactGA from "react-ga4";
+
+// // 🔹 Replace with your GA4 Measurement ID
+// ReactGA.initialize("G-6SRFLRZPN4");
 import TagManager from 'react-gtm-module';
 
 // Initialize GTM
